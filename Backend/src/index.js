a3394708a2e0669ac81ce0525f02f289cc991410
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-01 23:50:45 +0530

    created network util and app

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,15 +1,27 @@
 // require('dotenv').config({path: '../../env'});
 import dotenv from "dotenv";
-
-import express from "express";
 import connectDB from "./db/index.db.js";
 
 dotenv.config({
     path:'../.env'
 });
 
-// console.log(process.env.PORT);
+const port = process.env.PORT || 3000
+
 connectDB()
+.then( () => {
+    app.on("error", (error) => {
+        console.log("errrror:", error);
+        throw error
+    })
+    app.listen(port, () => {
+        console.log(`Server is running at port: ${port}`);
+    })
+})
+.catch( (error) => {
+    console.log("Mongo DB connection failed !!!", error);
+
+})
 
 // const app= express();
 
---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,6 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve( requestHandler(req, res, next))
+        .catch( (error) => next(error))
+    }
+}
