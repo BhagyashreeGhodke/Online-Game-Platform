8592069202115f33f9e9ac29de495b303eb1dddf
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-02 16:23:53 +0530

    error handlers created

---
+++ Backend/src/utils/apiResponse.js
@@ -0,0 +1,12 @@
+class ApiResponse {
+    constructor(
+        statusCode,
+        data,
+        message = "Suceess"
+    ) {
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
