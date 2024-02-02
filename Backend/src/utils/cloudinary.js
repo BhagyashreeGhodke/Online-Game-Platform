bc574a27e328b0acbb0817e0ee14aee57d642057
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-02 19:02:14 +0530

    cloudinary connected

---
+++ Backend/src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret:  process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        const response= await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("file is uploaded on cloudinary");
+        console.log("response.url: ", response.url);
+        console.log("response: ", response);
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove locally saved temp file : upload operation failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
