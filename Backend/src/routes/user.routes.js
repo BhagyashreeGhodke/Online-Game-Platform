5219094818cf1741cc9dd810c4d251363cacbfb6
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-03 20:27:41 +0530

    updated

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import { upload }  from "../middlewares/multer.middlewares.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.single('avatar'),
+    registerUser
+    )
+
 // router.route("/login").post(login)
 
 export default router
