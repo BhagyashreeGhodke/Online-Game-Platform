40abdb6b19568fb3a3a2d5efcd2ecf942655ed43
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-02-04 18:44:05 +0530

    login logout routes added

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
 import { upload }  from "../middlewares/multer.middlewares.js"
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 
@@ -11,6 +12,10 @@ maxCount: 1}]),
     registerUser
     )
 
-// router.route("/login").post(login)
+router.route("/login").post(loginUser)
+
+//secured routes
+
+router.route("/logout").post( verifyJWT, logoutUser)
 
 export default router
